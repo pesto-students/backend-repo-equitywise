f378be29d46565373f54fd540d5f6275b70d3f1c
Shrishyle Pandit <64643330+shrishyle@users.noreply.github.com>
2024-05-19 22:16:01 +0530

    Installed JWT

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
 const app = require("express")();
-
 const http = require("http").Server(app);
-
 const mongoose = require("mongoose");
 mongoose.connect("mongodb+srv://shrishylepandit:b9uB6Hm730O8wzMY@equitywise.1kzmnnq.mongodb.net/?retryWrites=true&w=majority&appName=Equitywise");
-
 const User = require("./models/userModel");
 
 async function insert() {
@@ -13,7 +10,6 @@ async function insert() {
     email: "krishna@gmail.com",
   });
 }
-
 insert();
 
 http.listen(3000, () => {

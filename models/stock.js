713f87779b3a7e934c612cc46519a8edfa4c90f8
pranathi3105 <subhasuresh.y@gmail.com>
2024-06-21 10:35:44 +0530

    Get protfolio code added

--- models/stock.js
+++ models/stock.js
@@ -58,4 +58,20 @@ async function stockInsert(req, res) {
         res.status(500).json({ error: error.message });
     }
 }
-module.exports = { Stock, Portfolio ,stockInsert};
+
+async function getStock(req,res){
+    const { userId } = req.query;
+   try{
+    let portfolio = await Portfolio.findOne({ user: userId });
+    if (!portfolio) {
+        return res.status(404).json({ error: 'Portfolio not found' });
+        }
+        res.status(200).json(portfolio);
+        }catch(error){
+            console.error("error while fetching data from stock",error.message);
+            res.status(500).json({ error: error.message });
+        }
+
+   }
+
+module.exports = { Stock, Portfolio ,stockInsert, getStock};

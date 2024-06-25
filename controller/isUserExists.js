151b35cbf27b3d6a171d1de5e194a37e4925c35b
pranathi3105 <subhasuresh.y@gmail.com>
2024-06-25 22:42:57 +0530

    error message changes done

--- controller/isUserExists.js
+++ controller/isUserExists.js
@@ -10,9 +10,9 @@ async function FindUser(req,res){
         console.log("mailid ", emailid);
         const user = await authService.login(emailid);
         if (user) {
-            return res.status(200).json({ message: 'User doesnot exists in Server' });
+            return res.status(200).json({ message: 'User exists in Server' });
         }
-        return res.status(201).json({ message: 'User exists in Server' });
+        return res.status(201).json({ message: 'User does not exists in Server' });
     }
 
     catch(err){

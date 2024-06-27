27adb2fe637a554f39aaadae6b3c5d65fc7a016f
pranathi3105 <subhasuresh.y@gmail.com>
2024-06-27 21:58:12 +0530

    access controls added

--- app.js
+++ app.js
@@ -33,6 +33,12 @@ app.use(getWishlisRrouter);
 app.use(insertWishlistRouter);
 app.use(updateWishlistRouter);
 app.use(deleteWishlistRouter);
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  next();
+});
 const PORT = process.env.port || 5000;
 app.listen(PORT, () => {
 console.log(`Server is running on port ${PORT}`);

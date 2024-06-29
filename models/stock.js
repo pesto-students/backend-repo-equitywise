ddab8c450ece61a3bd9d9e6c4002215a8a5a1b3a
pranathi3105 <subhasuresh.y@gmail.com>
2024-06-29 10:50:39 +0530

    condition added to check shares greaterthan

--- models/stock.js
+++ models/stock.js
@@ -129,11 +129,17 @@ async function getStock(req,res){
 
         // Update stock fields
         stock.symbol = symbol;
-        stock.shares = stock.shares - shares;
+        if(stock.shares > shares){
+            stock.shares = stock.shares - shares;
         // Save the portfolio
         await portfolio.save();
 
         res.status(200).json(portfolio);
+        }
+        else{
+            return res.status(404).json({ error: 'Stocks is not suffiecient' });
+        }
+        
     } catch (error) {
         console.error('Error in Sell stock:', error.message);
         res.status(500).json({ error: error.message });
